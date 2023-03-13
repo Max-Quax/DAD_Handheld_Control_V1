6f431bebf5778d06cbe781f8557011b021e9d1b0
Max <maximilienjle@vt.edu>
2023-03-13 01:55:47 -0400

    Simplified interface. Added write to microSD functionality

--- DAD_Handheld_Control_V1/DAD_Interface_Handler.c
+++ DAD_Handheld_Control_V1/DAD_Interface_Handler.c
@@ -2,46 +2,87 @@
  * DAD_Interface_Handler.c
  *
  *  Created on: Mar 9, 2023
- *      Author: Max
+ *      Authors: Will Katz, Max Engel
  */
 
 #include "DAD_Interface_Handler.h"
 #include <string.h>
 
+// Initializes UART, timers necessary
+void initInterfaces(DAD_Interface_Struct* interfaceStruct){
+    // RSA Interface init
+    DAD_UART_Set_Config(RSA_BAUD, EUSCI_A0_BASE, &(interfaceStruct->RSA_UART_struct));
+    DAD_UART_Init(&interfaceStruct->RSA_UART_struct, RSA_BUFFER_SIZE);
+
+    // HMI Interface init
+        // TODO test that this works
+    DAD_UART_Set_Config(HMI_BAUD, EUSCI_A2_BASE, &interfaceStruct->HMI_UART_struct);
+    DAD_UART_Init(&interfaceStruct->HMI_UART_struct, HMI_BUFFER_SIZE);
+
+    // microSD Interface init, open log file
+    interfaceStruct->currentPort = STOP;                        // Describes what file is currently being written to
+    strcpy(interfaceStruct->fileName, "log.txt");
+    DAD_microSD_InitUART(&interfaceStruct->microSD_UART);
+    DAD_microSD_openFile(interfaceStruct->fileName, &interfaceStruct->microSD_UART);
+//    char fileName[] = "msptest.csv"; // debug
+//    char *message[] = {"header one", "header two"};
+//    DAD_microSD_Write_CSV(fileName, message, 2, &microSD_UART);
+
+    // Wakeup Timer Init
+    DAD_Timer_Initialize_ms(FSM_TIMER_PERIOD, FSM_TIMER_HANDLE, &(interfaceStruct->FSMtimerConfig));
+}
+
+void writeToPeriphs(DAD_Interface_Struct* interfaceStruct){
+    // Read individual packets from buffer
+    while(PACKET_SIZE <= DAD_UART_NumCharsInBuffer(&interfaceStruct->RSA_UART_struct)){
+        handlePacket(interfaceStruct);
+
+        // TODO handle vib/sound
+    }
+
+    // Flush out remaining chars from buffer
+    while(DAD_UART_HasChar(&interfaceStruct->RSA_UART_struct)){
+        DAD_UART_GetChar(&interfaceStruct->RSA_UART_struct);
+    }
+}
+
 static void *intToString(int num) {
-    char str[PACKET_SIZE];
+    char str[PACKET_SIZE+1];
     sprintf(str, "%d", num);
     return str;
 }
 
-void handlePacket(DAD_UART_Struct* RSA_UARTptr, DAD_UART_Struct* HMI_UARTptr)
+
+static void handlePacket(DAD_Interface_Struct* interfaceStruct)
 {
     // Construct packet
-    uint8_t packet[PACKET_SIZE];
-    constructPacket(packet, RSA_UARTptr);
+    uint8_t packet[PACKET_SIZE+1];
+    constructPacket(packet, &interfaceStruct->RSA_UART_struct);
 
     // Interpret packet
     packetStatus PKstatus = (packetStatus)((packet[0] & STATUS_MASK) >> 3);
     packetType type = (packetType)(packet[0] & PACKET_TYPE_MASK);
-    int port = (packet[0] & PACKET_SIZE) >> 5;
+    int port = (packet[0] & PORT_MASK) >> 5;
     int data;
 
     // Deal with packet
     switch(PKstatus)
     {
         case DISCON:
-            sensorDisconnectHMI(port, HMI_UARTptr);
+            sensorDisconnectHMI(port, &interfaceStruct->HMI_UART_struct);
             break;
         case CON_D:
             switch(type)
             {
                 case TEMP:
                     data = (packet[1] << 8) + packet[2];
-                    writeToHMI(port, data%100, type, HMI_UARTptr);
+                    writeToHMI(port, data, type, &interfaceStruct->HMI_UART_struct);
+                    writeToMicroSD(port, data, type, interfaceStruct);
                     break;
                 case HUM:
                     data = (packet[1] << 8) + packet[2];
-                    writeToHMI(port, data%100, type, HMI_UARTptr);
+                    writeToHMI(port, data%100, type, &interfaceStruct->HMI_UART_struct);
+                    writeToMicroSD(port, data%100, type, interfaceStruct);
                     break;
                 case VIB:
                     break;
@@ -50,16 +91,19 @@ void handlePacket(DAD_UART_Struct* RSA_UARTptr, DAD_UART_Struct* HMI_UARTptr)
             }
             break;
         case CON_ND:
+            DAD_microSD_Write("connected a sensor\n", &interfaceStruct->microSD_UART);  // TODO
             break;
         case MSG:
+            DAD_microSD_Write("message\n", &interfaceStruct->microSD_UART);             // TODO
             break;
+        default:
+            DAD_microSD_Write("bad packet\n", &interfaceStruct->microSD_UART);
     }
 }
 
-
 // Constructs packet from data in UART HAL's ring buffer
     // Buffer was originally constructed as a FIFO ring buffer.
-bool constructPacket(uint8_t* packet, DAD_UART_Struct* UARTptr){
+static bool constructPacket(uint8_t* packet, DAD_UART_Struct* UARTptr){
     // Remove any "end packet" characters
     char c = 255;
     while(c == 255 && DAD_UART_HasChar(UARTptr)){
@@ -67,7 +111,7 @@ bool constructPacket(uint8_t* packet, DAD_UART_Struct* UARTptr){
     }
 
     // Construct Packet
-    if(DAD_UART_NumCharsInBuffer(UARTptr) > PACKET_SIZE){   // Check number of chars in buffer
+    if(DAD_UART_NumCharsInBuffer(UARTptr) >= PACKET_SIZE){   // Check number of chars in buffer
         int i;
         for(i = 0; i < PACKET_SIZE; i++){
             packet[i] = c;
@@ -78,7 +122,7 @@ bool constructPacket(uint8_t* packet, DAD_UART_Struct* UARTptr){
     return false;
 }
 
-void writeToHMI(uint8_t port, uint8_t data, packetType type, DAD_UART_Struct* UARTptr)
+static void writeToHMI(uint8_t port, uint8_t data, packetType type, DAD_UART_Struct* UARTptr)
 {
     DAD_UART_Write_Str(UARTptr, "HOME.s");
     DAD_UART_Write_Char(UARTptr, port+49);
@@ -105,7 +149,28 @@ void writeToHMI(uint8_t port, uint8_t data, packetType type, DAD_UART_Struct* UA
     return;
 }
 
-void sensorDisconnectHMI(int port, DAD_UART_Struct* UARTptr)
+static void writeToMicroSD(uint8_t port, uint8_t data, packetType type, DAD_Interface_Struct* interfaceStruct){
+    // Check that we are writing to the right file
+    if(interfaceStruct->currentPort != port){           // Compare data type to the file that is currently being written to
+        // TODO open the correct file
+        // sprintf(fileName, "%d.txt", port);
+        //switch(*currentPort){
+            //
+        //}
+
+        interfaceStruct->currentPort = port;
+    }
+
+    // Construct message
+    char message[MESSAGE_LEN];
+    //sprintf(message, "%d,%d", (int)packetType, data);
+    sprintf(message, "%d,%d\n", data, data);   // debug
+
+    // Write data to microSD
+    DAD_microSD_Write(message, &interfaceStruct->microSD_UART);
+}
+
+static void sensorDisconnectHMI(int port, DAD_UART_Struct* UARTptr)
 {
     // Report sensor disconnected to HMI
     DAD_UART_Write_Str(UARTptr, "HOME.s");
--- DAD_Handheld_Control_V1/DAD_Interface_Handler.h
+++ DAD_Handheld_Control_V1/DAD_Interface_Handler.h
@@ -2,7 +2,7 @@
  * DAD_Packet_Handler.h
  *
  *  Created on: Mar 9, 2023
- *      Author: Max
+ *      Author: Will Katz, Max Engel
 
     Meant to abstract the interfaces away.
     End goal is to have control software deal with data at a packet level.
@@ -12,32 +12,64 @@
 #ifndef DAD_INTERFACE_HANDLER_H_
 #define DAD_INTERFACE_HANDLER_H_
 
+// Standard includes
+#include <string.h>
+#include <stdio.h>
+
 // HAL Includes
 #include "HAL/DAD_UART.h"
 #include "HAL/DAD_microSD.h"
 
-// Macros
+// UART Macros
+#define RSA_BAUD 9600
+#define RSA_BUFFER_SIZE 1024
+#define HMI_BAUD 9600
+#define HMI_BUFFER_SIZE 1024
+
+// Timer Macros
+#define FSM_TIMER_HANDLE TIMER_A0_BASE
+#define FSM_TIMER_PERIOD 500            // Period in ms
+
+// Packet Macros
 #define STATUS_MASK 24
 #define PACKET_TYPE_MASK 7
 #define PORT_MASK 224
 #define PACKET_SIZE 5
+#define MESSAGE_LEN (sizeof(char)*(PACKET_SIZE - 1) + 1)
 
 typedef enum {DISCON, CON_D, CON_ND, MSG} packetStatus;
 typedef enum {TEMP, HUM, VIB, MIC, LOWBAT, ERR, STOP, START} packetType;
 
-// Report to HMI that sensor has been disconnected
-void sensorDisconnectHMI(int port, DAD_UART_Struct* UARTptr);
+typedef struct DAD_Interface_Struct_{
+    DAD_UART_Struct RSA_UART_struct;
+    DAD_UART_Struct HMI_UART_struct;
+    DAD_UART_Struct microSD_UART;
+    Timer_A_UpModeConfig FSMtimerConfig;
+    uint8_t currentPort;  // Describes what file is currently being written to
+    char fileName[13];
+} DAD_Interface_Struct;
 
-// Write data to HMI
-void writeToHMI(uint8_t port, uint8_t data, packetType type, DAD_UART_Struct* UARTptr);
+// Initializes UART, timers necessary
+void initInterfaces(DAD_Interface_Struct* interfaceStruct);
 
-// TODO write data to microSD
-
-// Handle an individual packet
-void handlePacket(DAD_UART_Struct* RSA_UARTptr, DAD_UART_Struct* HMI_UARTptr);
+// Write all elements from RSA UART buffer to peripherals
+void writeToPeriphs(DAD_Interface_Struct* interfaceStruct);
 
 // Build packet from data in UART buffer
-bool constructPacket(uint8_t* packet, DAD_UART_Struct* UARTptr);
+static bool constructPacket(uint8_t* packet, DAD_UART_Struct* UARTptr);
 
+// Handle an individual packet
+static void handlePacket(DAD_Interface_Struct* interfaceStruct);
+
+// Write single packet of data to HMI
+static void writeToHMI(uint8_t port, uint8_t data, packetType type, DAD_UART_Struct* UARTptr);
+
+// Writes single packet of data to microSD
+static void writeToMicroSD(uint8_t port, uint8_t data, packetType type, DAD_Interface_Struct* interfaceStruct);
+
+// Report to HMI that sensor has been disconnected
+static void sensorDisconnectHMI(int port, DAD_UART_Struct* UARTptr);
+
+// TODO write commands to RSA
 
 #endif /* DAD_INTERFACE_HANDLER_H_ */
